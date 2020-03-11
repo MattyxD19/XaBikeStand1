5a35aa87d9d10519a0c885b70851404c0489f479
MattyxD19 <mathiasbc97@gmail.com>
2020-03-11 15:02:16 +0100

    added buttons with commands to the map as well as a User model and a BikeRack model

---
+++ XaBikeStand/XaBikeStand/Models/BikeRack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XaBikeStand.Models
+{
+    class BikeRack
+    {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public int Longitude { get; set; }
+        public int Latitude { get; set; }
+    }
+}
