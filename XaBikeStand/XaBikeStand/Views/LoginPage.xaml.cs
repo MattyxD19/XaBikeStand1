c920d97ea5079a69345b8924e2cc317bbaf39cb5
Alexandra Reiter <40567095+alexreiter@users.noreply.github.com>
2020-03-17 21:20:06 +0100

    Add files via upload

---
+++ XaBikeStand/XaBikeStand/Views/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Login.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void SignUp_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+
+        void Login_Clicked(object sender, System.EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulation", "Login Successful", "Yes", "Cancel");
+
+
+                if (result)
+                    await Navigation.PushAsync(new HomePage());
+
+
+            });
+
+        }
+    }
+}
---
+++ XaBikeStand/XaBikeStand/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Login.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //User user = new User(UserName.Text, Password.Text, Email.Text);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulation", "Sign Up Successful", "Yes", "Cancel");
+
+                if (result)
+                    await Navigation.PushAsync(new LoginPage());
+            });
+        }
+    }
+}
