d0ba29c3aab30cc4fa1968bef5bb83aa411bbdb0
Mathias Christensen <mathiasbc97@gmail.com>
2020-03-17 16:13:11 +0100

    trying to fix the mapview

--- XaBikeStand/XaBikeStand/ViewModels/MapViewModel.cs
+++ XaBikeStand/XaBikeStand/ViewModels/MapViewModel.cs
@@ -3,22 +3,72 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using XaBikeStand.Models;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace XaBikeStand.ViewModels
 {
     class MapViewModel : BaseViewModel, INotifyPropertyChanged
-    {       
+    {
+
+        BikeStation bikeStation = new BikeStation();
+        ServerClient serverClient = new ServerClient();
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public List<Pin> pinCollection = new List<Pin>();
 
-       
+        public MapViewModel()
+        {
+            AddPins();
+        }
+
+        private void AddPins()
+        {
+            pinCollection.Add(new Pin { Label = "bikeStation.Address", Address = "Cykler fri: " + "bikeStation.Bikes", Type = PinType.Place, Position = new Position(54.913197, 9.778213) });
+            pinCollection.Add(new Pin { Label = "bikeStation.Address", Address = "Cykler fri: " + "bikeStation.Bikes", Type = PinType.Place, Position = new Position(54.919999, 9.807773) });
+
+            
+
+        }
+
+        #region --Binding--
+
+
+        private Pin _pin;
+
+        public Pin pin
+        {
+            get
+            {
+                return _pin;
+            }
+            set
+            {
+                _pin = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        private void GetBikeStations()
+        {
+            //serverClient.
+
+            //pinCollection.Add(new Pin { Label = bikeStation.Address, Address = "Cykler fri: " + bikeStation.Bikes, Type = PinType.Place, Position = new Position(bikeStation.Longitude, bikeStation.Latitude) });
+
+        }
+
         public void PinClicked(object sender, EventArgs e)
         {
 
         }
-        
+
+        private void PinInfo(object sender, EventArgs e)
+        {
+            //menu = new PopUpMenu(this, show)
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
--- XaBikeStand/XaBikeStand/Views/MapView.xaml.cs
+++ XaBikeStand/XaBikeStand/Views/MapView.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XaBikeStand.Models;
 using XaBikeStand.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
@@ -19,30 +21,22 @@ namespace XaBikeStand.Views
          * Breaking the MVVM pattern since it was hard to bind the map
          * -Mathias
          */
+        public List<Pin> listPins = new List<Pin>();
 
         public MapView()
         {
             InitializeComponent();
             Mapcontent();
+            
+            foreach (var pin in listPins)
+            {
+                AppMap.Pins.Add(pin);
+            }
             Content = AppMap;
+            
         }
 
-        Pin alsionPin = new Pin
-        {
-            Label = "Alsion",
-            Address = "Alsion",
-            Type = PinType.Place,
-            Position = new Position(54.912794, 9.779231)
-        };
 
-        Pin FitnessWorldPin = new Pin
-        {
-            Label = "Fitness World",
-            Address = "Fitness World",
-            Type = PinType.Place,
-            Position = new Position(54.919936, 9.807708),
-            
-        };
 
         Map AppMap = new Map
         {
@@ -51,18 +45,14 @@ namespace XaBikeStand.Views
             
         };
 
+        
+
+
         public void Mapcontent()
         {
             Position position = new Position(54.912794, 9.779231);
             MapSpan startSpan = new MapSpan(position, 0.01, 0.01);
             AppMap.MoveToRegion(startSpan);
-
-            AppMap.Pins.Add(alsionPin);
-            
-            AppMap.Pins.Add(FitnessWorldPin);
-
-            
-            
         }
 
 
