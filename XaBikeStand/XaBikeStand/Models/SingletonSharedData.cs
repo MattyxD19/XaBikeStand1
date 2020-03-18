ef1208b476ce0db9553f995ab816e70347bdbf04
Rasmusp97 <Rasmusp97@hotmail.com>
2020-03-19 00:00:56 +0100

    Style changes and more implementation of login

---
+++ XaBikeStand/XaBikeStand/Models/SingletonSharedData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XaBikeStand.Models
+{
+    public sealed class SingletonSharedData
+    {
+        private static SingletonSharedData instance = null;
+        private static readonly object padlock = new object();
+
+        public User LoggedInUser { get; set; }
+
+
+        private SingletonSharedData()
+        {
+        }
+
+        /// <summary>
+        /// The method creates an instance of the SharedData class or returns the existing instance
+        /// if it has already been created. 
+        /// </summary>
+        /// <returns>Returns the SharedData object</returns>
+        public static SingletonSharedData GetInstance()
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonSharedData();
+                }
+                return instance;
+            }
+        }
+    }
+}
